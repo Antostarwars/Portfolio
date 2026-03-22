b2ef3683917a8811b6e9b2be52e147b2dee52ebd
Antostarwars <derosaantonio587@gmail.com>
2026-03-22 02:44:48 +0100

    Fixed static builds for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://antostarwars.xyz',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
+  redirects: {
+    '/': '/en',
+  },
   vite: {
     plugins: [tailwindcss()]
   }
